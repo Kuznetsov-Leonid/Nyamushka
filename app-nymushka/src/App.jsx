8835e69833596b97795f76102b5fefa360bcd49e
orlan1211 <orlan1211@yahoo.com>
2022-06-18 02:27:42 +0300

    Main

--- app-nymushka/src/App.jsx
+++ app-nymushka/src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import './style/index.css'
 import './style/container.css';
-import Title from './Components/Title/Title';
-import {CardItem} from './Components/Card/CardItem';
+import Header from './Components/Title/Title';
+import Main from "./Components/Main/Main";
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
       <div className = "app">
-        
+        <div className = "inner-app">
+          <div className = "Container">
+            <Header/>
+            <Main/>
+          </div>
+        </div>
       </div>
     </>
   );
