7dea8ccfd90bdb0145a35536d4338811cba8eb6f
orlan1211 <orlan1211@yahoo.com>
2022-06-17 23:17:08 +0300

    Card item

--- app-nymushka/src/App.jsx
+++ app-nymushka/src/App.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import './style/index.css'
 import './style/container.css';
 import Title from './Components/Title/Title';
+import {CardItem} from './Components/Card/CardItem';
 
 function App() {
   return (
     <>
       <div className = "app">
-        <div className = "container">
-          <Title/>
-        </div>
+        
       </div>
     </>
   );
