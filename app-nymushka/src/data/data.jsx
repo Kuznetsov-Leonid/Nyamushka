0c079c1ece43a83e7e8c99f0929973de5570aa3f
orlan1211 <orlan1211@yahoo.com>
2022-06-17 23:37:40 +0300

    Make the data

---
+++ app-nymushka/src/data/data.jsx
@@ -0,0 +1,10 @@
+/**
+ * create by Kuznetsov Leonid
+ * 
+ * DATA Base with store
+ */
+export const DATA = [
+    {id:1, nameTitle: "с фуа-гра", portions: 10, mous: "", present: "мышь в падарок", message: "", weight: 0.5},
+    {id:2, nameTitle: "с рыбой", portions: 40, mous: 2, present: "мыши в падарок", message: "", weight: 2},
+    {id:3, nameTitle: "с курой", portions: 100, mous: 5, present: "мышей в падарок", message: "заказчик доволен", weight: 5},
+];
